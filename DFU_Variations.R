c46f9bf08841f60fee35b1cb4c844dabd89d23d5
wm-watson <wpwatson@uams.edu>
2024-06-20 16:27:22 -0700

    DFU

--- DFU_Variations.R
+++ DFU_Variations.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 #Data----
-dm_summary <- read_csv("C:/Users/watso/Box/PhD/HPMT 6213 - Variation in Health System Performance/Diabetic Disparities/Data/prov_pat_zip/prov_pat_zip.csv",
+dm_summary <- read_csv("/Users/williamwatson/Library/CloudStorage/Box-Box/PhD/HPMT 6213 - Variation in Health System Performance/Diabetic Disparities/Data/prov_pat_zip/prov_pat_zip.csv",
                        col_types = cols(
                          PAT_ZIP_5 = col_character()  # Adjust accordingly if there are more columns
                        ))
